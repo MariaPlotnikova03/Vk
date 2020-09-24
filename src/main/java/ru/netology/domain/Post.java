416afdaf64bbc72cbf7797b0fd47e9addd003681
Maria Plotnikova <plotnikova.ma03@yandex.ru>
2020-09-24 20:42:28 +0300

    Initial commit

---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,103 @@
+package ru.netology.domain;
+
+public class Post {
+    private String id;
+    private String userId;
+    private String namePost;
+    private String date;
+    private String imageUrl;
+    private String info;
+    private String commentsInfo;
+    private int commentsQuantity;
+    private int views;
+    private int reposts;
+    private int likes;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getNamePost() {
+        return namePost;
+    }
+
+    public void setNamePost(String namePost) {
+        this.namePost = namePost;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    public String getCommentsInfo() {
+        return commentsInfo;
+    }
+
+    public void setCommentsInfo(String commentsInfo) {
+        this.commentsInfo = commentsInfo;
+    }
+
+    public int getCommentsQuantity() {
+        return commentsQuantity;
+    }
+
+    public void setCommentsQuantity(int commentsQuantity) {
+        this.commentsQuantity = commentsQuantity;
+    }
+
+    public int getViews() {
+        return views;
+    }
+
+    public void setViews(int views) {
+        this.views = views;
+    }
+
+    public int getReposts() {
+        return reposts;
+    }
+
+    public void setReposts(int reposts) {
+        this.reposts = reposts;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
+}
