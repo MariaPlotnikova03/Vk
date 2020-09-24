0abcb1f69d3838430453dc487f17245df2d6d2ba
Maria Plotnikova <plotnikova.ma03@yandex.ru>
2020-09-24 22:49:30 +0300

    Manager

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,19 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private PostManager postmanager;
+
+    // метод поиска поиска по стене
+    public Post[] wallSearch(int ownerId, String domain, String query, int ownersOnly, int count, int offset) {
+        // TODO: add logic
+        return null;
+    }
+
+    //метод удаления записи со стены
+    public void delete(int ownerId, int postId) {
+        // TODO: add logic
+    }
+
+}
