d19fff0a41b6fcdfaf55186b71c824f08a08c905
Maria Plotnikova <plotnikova.ma03@yandex.ru>
2020-09-24 21:46:26 +0300

    Classes and fields was added

--- src/main/java/ru/netology/domain/Post.java
+++ src/main/java/ru/netology/domain/Post.java
@@ -1,17 +1,29 @@
 package ru.netology.domain;
 
 public class Post {
-    private String id;
-    private String userId;
-    private String namePost;
-    private String date;
-    private String imageUrl;
-    private String info;
-    private String commentsInfo;
-    private int commentsQuantity;
-    private int views;
-    private int reposts;
-    private int likes;
+    private String id; //идентификатор записи
+    private String ownerId; //идентификатор владельца стены, на которой размещена запись
+    private String fromId; //идентификатор автора записи
+    private String createdBy; //идентификатор администратора, который опубликовал запись
+    private String date; //время публикации записи в формате unixtime
+    private String text; //текст записи
+    private String replyOwnerId; // идентификатор владельца записи, в ответ на которую была оставлена текущая
+    private String replyPostId; // идентификатор записи, в ответ на которую была оставлена текущая
+    private String friendsOnly; // 1, если запись была создана с опцией «Только для друзей»
+
+    private String copyright; //источник материала
+
+    private String postType; //тип записи
+
+    private String signerId; //идентификатор автора, если запись была опубликована от имени сообщества и подписана пользователем
+    private int canPin; // информация о том, может ли текущий пользователь закрепить запись (1 — может, 0 — не может)
+    private int canDelete; //информация о том, может ли текущий пользователь удалить запись (1 — может, 0 — не может)
+    private int canEdit; //информация о том, может ли текущий пользователь редактировать запись (1 — может, 0 — не может)
+    private int isPinned; //информация о том, что запись закреплена
+    private int markedAsAds; //информация о том, содержит ли запись отметку "реклама" (1 — да, 0 — нет)
+    private boolean isFavorite; //true, если объект добавлен в закладки у текущего пользователя
+    private int postponedId; //идентификатор отложенной записи. Это поле возвращается тогда, когда запись стояла на таймере
+
 
     public String getId() {
         return id;
@@ -21,20 +33,28 @@ public class Post {
         this.id = id;
     }
 
-    public String getUserId() {
-        return userId;
+    public String getOwnerId() {
+        return ownerId;
     }
 
-    public void setUserId(String userId) {
-        this.userId = userId;
+    public void setOwnerId(String ownerId) {
+        this.ownerId = ownerId;
     }
 
-    public String getNamePost() {
-        return namePost;
+    public String getFromId() {
+        return fromId;
     }
 
-    public void setNamePost(String namePost) {
-        this.namePost = namePost;
+    public void setFromId(String fromId) {
+        this.fromId = fromId;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
     }
 
     public String getDate() {
@@ -45,59 +65,115 @@ public class Post {
         this.date = date;
     }
 
-    public String getImageUrl() {
-        return imageUrl;
+    public String getText() {
+        return text;
     }
 
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
+    public void setText(String text) {
+        this.text = text;
     }
 
-    public String getInfo() {
-        return info;
+    public String getReplyOwnerId() {
+        return replyOwnerId;
     }
 
-    public void setInfo(String info) {
-        this.info = info;
+    public void setReplyOwnerId(String replyOwnerId) {
+        this.replyOwnerId = replyOwnerId;
     }
 
-    public String getCommentsInfo() {
-        return commentsInfo;
+    public String getReplyPostId() {
+        return replyPostId;
     }
 
-    public void setCommentsInfo(String commentsInfo) {
-        this.commentsInfo = commentsInfo;
+    public void setReplyPostId(String replyPostId) {
+        this.replyPostId = replyPostId;
     }
 
-    public int getCommentsQuantity() {
-        return commentsQuantity;
+    public String getFriendsOnly() {
+        return friendsOnly;
     }
 
-    public void setCommentsQuantity(int commentsQuantity) {
-        this.commentsQuantity = commentsQuantity;
+    public void setFriendsOnly(String friendsOnly) {
+        this.friendsOnly = friendsOnly;
     }
 
-    public int getViews() {
-        return views;
+    public String getCopyright() {
+        return copyright;
     }
 
-    public void setViews(int views) {
-        this.views = views;
+    public void setCopyright(String copyright) {
+        this.copyright = copyright;
     }
 
-    public int getReposts() {
-        return reposts;
+    public String getPostType() {
+        return postType;
     }
 
-    public void setReposts(int reposts) {
-        this.reposts = reposts;
+    public void setPostType(String postType) {
+        this.postType = postType;
     }
 
-    public int getLikes() {
-        return likes;
+    public String getSignerId() {
+        return signerId;
     }
 
-    public void setLikes(int likes) {
-        this.likes = likes;
+    public void setSignerId(String signerId) {
+        this.signerId = signerId;
+    }
+
+    public int getCanPin() {
+        return canPin;
+    }
+
+    public void setCanPin(int canPin) {
+        this.canPin = canPin;
+    }
+
+    public int getCanDelete() {
+        return canDelete;
+    }
+
+    public void setCanDelete(int canDelete) {
+        this.canDelete = canDelete;
+    }
+
+    public int getCanEdit() {
+        return canEdit;
+    }
+
+    public void setCanEdit(int canEdit) {
+        this.canEdit = canEdit;
+    }
+
+    public int getIsPinned() {
+        return isPinned;
+    }
+
+    public void setIsPinned(int isPinned) {
+        this.isPinned = isPinned;
+    }
+
+    public int getMarkedAsAds() {
+        return markedAsAds;
+    }
+
+    public void setMarkedAsAds(int markedAsAds) {
+        this.markedAsAds = markedAsAds;
+    }
+
+    public boolean isFavorite() {
+        return isFavorite;
+    }
+
+    public void setFavorite(boolean favorite) {
+        isFavorite = favorite;
+    }
+
+    public int getPostponedId() {
+        return postponedId;
+    }
+
+    public void setPostponedId(int postponedId) {
+        this.postponedId = postponedId;
     }
 }
